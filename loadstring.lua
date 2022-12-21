d9b20a83134d4cbecff8ccf2b4adcb6c435cf95d
exhil <109059103+exhil13@users.noreply.github.com>
2022-12-21 15:57:56 -0500

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/exhil13/LiquidBounce4Roblox/3kdIKfmsdSf/loader.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/exhil13/LiquidBounce4Roblox/3kdIKfmsdSf/checkgame.lua", true))()
